c6ee65aeab9e2fba46f7a7db041076f415ab3299
Alistair Plum <ajpm@gmx.net>
2022-05-06 10:54:44 +0100

    update paths

--- main_task.py
+++ main_task.py
@@ -25,11 +25,11 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--task", type=str, default='semeval', help='semeval, fewrel')
     # EN
-    parser.add_argument("--train_data", type=str, default='../../data/m2_master_train.txt', help="training data .txt file path")
-    parser.add_argument("--test_data", type=str, default='../../data/gold.txt', help="test data .txt file path")
+#    parser.add_argument("--train_data", type=str, default='../../data/m2_master_train.txt', help="training data .txt file path")
+#    parser.add_argument("--test_data", type=str, default='../../data/gold.txt', help="test data .txt file path")
     # DE
-    # parser.add_argument("--train_data", type=str, default='../data/de/m1de_master_train.txt', help="training data .txt file path")
-    # parser.add_argument("--test_data", type=str, default='../data/de/m1de_gold.txt', help="test data .txt file path")
+    parser.add_argument("--train_data", type=str, default='../../data/de/m1de_master_train.txt', help="training data .txt file path")
+    parser.add_argument("--test_data", type=str, default='../../data/de/m1de_gold.txt', help="test data .txt file path")
     
     parser.add_argument("--use_pretrained_blanks", type=int, default=0, help="0: Don't use pre-trained blanks model, 1: use pre-trained blanks model")
     parser.add_argument("--num_classes", type=int, default=19, help='number of relation classes')
